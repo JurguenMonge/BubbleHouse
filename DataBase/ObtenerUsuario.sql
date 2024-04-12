960120eb9d2f92f4d017b2eb45fd7362d13eb73b
Victor120120 <103450911+Victor120120@users.noreply.github.com>
2024-04-11 20:47:40 -0600

    SP Sesiones y Usuario

---
+++ DataBase/ObtenerUsuario.sql
@@ -0,0 +1,29 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE Obtener_Usuario
+@Id_Producto int
+AS
+BEGIN
+    -- Seleccionar todos los productos cuyo estado sea diferente de 0
+    SELECT U.ID_USUARIO, U.DSC_NOMBRE, U.DSC_PRIMER_APELLIDO, U.DSC_SEGUNDO_APELLIDO, U.DSC_CORREO, U.DSC_PASSWORD, U.FEC_REGISTRO, U.DSC_TELEFONO, U.ESTADO
+    FROM TB_USUARIO AS U
+    WHERE ESTADO <> 0 AND U.ID_USUARIO = @Id_Producto;
+END
---
+++ DataBase/ObtenerUsuariosActivos.sql
@@ -0,0 +1,31 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE Obtener_Usuarios_Activos
+AS
+BEGIN
+    -- Seleccionar todos los productos cuyo estado sea diferente de 0
+    SELECT P.DSC_NOMBRE_PRODUCTO, P.DSC_DESCRIPCION, P.DSC_URL_IMAGEN, P.NUM_PRECIO, P.ESTADO, SUBC.ID_SUBCATE_PRODUCTO, SUBC.DSC_NOMBRE_SUBCATEGORIA, CATE.ID_CATE_PRODUCTO, CATE.DSC_NOMBRE_CATEGORIA
+    FROM TB_PRODUCTO AS P
+    INNER JOIN TB_SUBCATEGORIA_PRODUCTO AS SUBC ON P.ID_SUBCATE_PRODUCTO = SUBC.ID_SUBCATE_PRODUCTO
+    INNER JOIN TB_CATE_PRODUCTO AS CATE ON SUBC.ID_CATE_PRODUCTO_ID = CATE.ID_CATE_PRODUCTO
+    WHERE ESTADO <> 0;
+END
+GO
