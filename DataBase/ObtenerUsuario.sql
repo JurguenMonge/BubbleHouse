1c8284facaf84176c9442499cbf724b0363f035c
JbDolmus <129348901+JbDolmus@users.noreply.github.com>
2024-04-12 15:53:30 -0600

    Update ObtenerUsuario.sql

--- DataBase/ObtenerUsuario.sql
+++ DataBase/ObtenerUsuario.sql
@@ -19,11 +19,11 @@ GO
 -- Description:	<Description,,>
 -- =============================================
 CREATE PROCEDURE Obtener_Usuario
-@Id_Producto int
+@Id_Usuario int
 AS
 BEGIN
-    -- Seleccionar todos los productos cuyo estado sea diferente de 0
+    -- Seleccionar todos los usuarios cuyo estado sea diferente de 0
     SELECT U.ID_USUARIO, U.DSC_NOMBRE, U.DSC_PRIMER_APELLIDO, U.DSC_SEGUNDO_APELLIDO, U.DSC_CORREO, U.DSC_PASSWORD, U.FEC_REGISTRO, U.DSC_TELEFONO, U.ESTADO
     FROM TB_USUARIO AS U
-    WHERE ESTADO <> 0 AND U.ID_USUARIO = @Id_Producto;
+    WHERE ESTADO <> 0 AND U.ID_USUARIO = @Id_Usuario;
 END
