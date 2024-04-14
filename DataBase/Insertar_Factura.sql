932d071f6f08093a0a429b99b7faa9e18db32ef2
JbDolmus <129348901+JbDolmus@users.noreply.github.com>
2024-04-14 12:00:21 -0600

    Update Insertar_Factura.sql

--- DataBase/Insertar_Factura.sql
+++ DataBase/Insertar_Factura.sql
@@ -16,60 +16,64 @@ CREATE PROCEDURE Insertar_Factura
 AS
 BEGIN
     BEGIN TRY
-        BEGIN TRANSACTION 
 
         IF NOT EXISTS (SELECT * FROM TB_PRODUCTO WHERE ID_PRODUCTO = @Id_Producto)
         BEGIN
             SET @IDRETURN = -1;
             SET @ERRORID = 1;
             SET @ERRORDESCRIPCION = 'El producto especificado no existe.';
-            RETURN; -- Salir del procedimiento almacenado
         END
-
-		IF NOT EXISTS (SELECT * FROM TB_SESION WHERE ID_SESION = @Id_Sesion)
+		ELSE IF NOT EXISTS (SELECT * FROM TB_SESION WHERE ID_SESION = @Id_Sesion)
         BEGIN
             SET @IDRETURN = -1;
             SET @ERRORID = 1;
             SET @ERRORDESCRIPCION = 'La sesión especificada no existe.';
-            RETURN; -- Salir del procedimiento almacenado
         END
-
-        IF @Num_Subtotal IS NULL OR LEN(@Num_Subtotal) = 0
+        ELSE IF @Num_Subtotal IS NULL OR LEN(@Num_Subtotal) = 0
         BEGIN
             SET @IDRETURN = -1;
             SET @ERRORID = 1;
             SET @ERRORDESCRIPCION = 'El subtotal es nulo o está vacío.';
-            RETURN; -- Salir del procedimiento almacenado
         END
-
-        IF @Num_Descuento IS NULL OR LEN(@Num_Descuento) = 0
+        ELSE IF @Num_Descuento IS NULL OR LEN(@Num_Descuento) = 0
         BEGIN
             SET @IDRETURN = -1;
             SET @ERRORID = 1;
             SET @ERRORDESCRIPCION = 'El descuento es nulo o está vacío.';
-            RETURN; -- Salir del procedimiento almacenado
         END
-
-        IF @Num_Total IS NULL OR LEN(@Num_Descuento) = 0
+        ELSE IF @Num_Total IS NULL OR LEN(@Num_Descuento) = 0
         BEGIN
             SET @IDRETURN = -1;
             SET @ERRORID = 1;
             SET @ERRORDESCRIPCION = 'El total es nulo o está vacío.';
-            RETURN; -- Salir del procedimiento almacenado
         END
+        ELSE
+        BEGIN
+            INSERT INTO TB_FACTURA(ID_PRODUCTO, ID_SESION, NUM_SUBTOTAL, NUM_DESCUENTO, NUM_TOTAL, FECHA, ESTADO)
+            VALUES (@Id_Producto, @Id_Sesion, @Num_Subtotal, @Num_Descuento, @Num_Total, GETUTCDATE(), 1);
 
-        INSERT INTO TB_FACTURA(ID_PRODUCTO, ID_SESION, NUM_SUBTOTAL, NUM_DESCUENTO, NUM_TOTAL, FECHA, ESTADO)
-        VALUES (@Id_Producto, @Id_Sesion, @Num_Subtotal, @Num_Descuento, @Num_Total, GETUTCDATE(), 1);
-
-        SET @IDRETURN = SCOPE_IDENTITY();
-
-        COMMIT TRANSACTION 
+            SET @IDRETURN = SCOPE_IDENTITY();
+        END
     END TRY
     BEGIN CATCH
         SET @IDRETURN = -1;
         SET @ERRORID = ERROR_NUMBER();
         SET @ERRORDESCRIPCION = ERROR_MESSAGE();
-        ROLLBACK TRANSACTION 
+		--Bitacorear error en BD.
+		INSERT INTO TB_ERROR_EN_BASE_DATOS 
+			(
+				NUM_SEVERIVDAD,
+				STORE_PROCEDURE,
+				NUM_ERROR,
+				DSC_DESCRIPCION,
+				NUM_LINEA,
+				FEC_ERROR
+			) 
+			SELECT ERROR_SEVERITY(),
+				   ERROR_PROCEDURE(),
+				   ERROR_NUMBER(),
+				   ERROR_MESSAGE(),
+				   ERROR_LINE(),
+				   GETUTCDATE();
     END CATCH
 END
-GO
