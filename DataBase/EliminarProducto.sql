87e153bb996e1b2cfd23916e9bf451a86e645519
JbDolmus <129348901+JbDolmus@users.noreply.github.com>
2024-04-14 11:58:08 -0600

    Update EliminarProducto.sql

--- DataBase/EliminarProducto.sql
+++ DataBase/EliminarProducto.sql
@@ -1,58 +1,54 @@
--- ================================================
--- Template generated from Template Explorer using:
--- Create Procedure (New Menu).SQL
---
--- Use the Specify Values for Template Parameters 
--- command (Ctrl-Shift-M) to fill in the parameter 
--- values below.
---
--- This block of comments will not be included in
--- the definition of the procedure.
--- ================================================
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
--- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
-CREATE PROCEDURE Eliminar_Producto
-    @Id_Producto int,
+
+CREATE PROCEDURE [dbo].[Eliminar_Categoria_Producto]
+    @Id_Categoria_Producto int,
     @IDRETURN int OUTPUT,
     @ERRORID int OUTPUT,
     @ERRORDESCRIPCION nvarchar(max) OUTPUT
 AS
 BEGIN
     BEGIN TRY
-        BEGIN TRANSACTION 
-
-        -- Verificar si el producto existe
-        IF NOT EXISTS (SELECT * FROM TB_PRODUCTO WHERE ID_PRODUCTO = @Id_Producto)
+        -- Verificar si la categor√≠a de producto existe
+        IF NOT EXISTS (SELECT * FROM TB_CATE_PRODUCTO WHERE ID_CATE_PRODUCTO = @Id_Categoria_Producto)
         BEGIN
             SET @IDRETURN = -1;
             SET @ERRORID = 12;
-            SET @ERRORDESCRIPCION = 'El producto especificado no existe.';
-            RETURN; -- Salir del procedimiento almacenado
+            SET @ERRORDESCRIPCION = 'La categor√≠a de producto especificada no existe.';
+            
         END
+		ELSE
+		BEGIN
+			-- Eliminar la categor√≠a de producto
+			UPDATE TB_CATE_PRODUCTO
+			SET ESTADO = 0
+			WHERE ID_CATE_PRODUCTO = @Id_Categoria_Producto;
 
-        -- Cambiar el estado del producto a 0 (desactivado)
-        UPDATE TB_PRODUCTO
-        SET ESTADO = 0
-        WHERE ID_PRODUCTO = @Id_Producto;
-
-        SET @IDRETURN = 1; -- …xito
-
-        COMMIT TRANSACTION 
+			SET @IDRETURN = 1; -- √âxito
+		END
     END TRY
     BEGIN CATCH
         SET @IDRETURN = -1;
         SET @ERRORID = ERROR_NUMBER();
         SET @ERRORDESCRIPCION = ERROR_MESSAGE();
-        ROLLBACK TRANSACTION 
-        -- AquÌ puedes manejar el error como prefieras, por ejemplo, lanzar una excepciÛn o registrar el error en una tabla de registro de errores.
+
+		--Bitacorear error en BD.
+		INSERT INTO TB_ERROR_EN_BASE_DATOS 
+			(
+				NUM_SEVERIVDAD,
+				STORE_PROCEDURE,
+				NUM_ERROR,
+				DSC_DESCRIPCION,
+				NUM_LINEA,
+				FEC_ERROR
+			) 
+			SELECT ERROR_SEVERITY(),
+				   ERROR_PROCEDURE(),
+				   ERROR_NUMBER(),
+				   ERROR_MESSAGE(),
+				   ERROR_LINE(),
+				   GETUTCDATE();
     END CATCH
 END
-GO
-
