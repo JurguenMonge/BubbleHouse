a24ab042f3bb9ae1b8f6dc50db6a9110e3d969d7
JbDolmus <129348901+JbDolmus@users.noreply.github.com>
2024-04-14 12:25:24 -0600

    Update Eliminar_Categoria_Producto.sql

--- DataBase/Eliminar_Categoria_Producto.sql
+++ DataBase/Eliminar_Categoria_Producto.sql
@@ -1,10 +1,9 @@
-
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE PROCEDURE Eliminar_Categoria_Producto
+CREATE PROCEDURE [dbo].[Eliminar_Categoria_Producto]
     @Id_Categoria_Producto int,
     @IDRETURN int OUTPUT,
     @ERRORID int OUTPUT,
@@ -12,31 +11,44 @@ CREATE PROCEDURE Eliminar_Categoria_Producto
 AS
 BEGIN
     BEGIN TRY
-        BEGIN TRANSACTION 
-
         -- Verificar si la categoría de producto existe
         IF NOT EXISTS (SELECT * FROM TB_CATE_PRODUCTO WHERE ID_CATE_PRODUCTO = @Id_Categoria_Producto)
         BEGIN
             SET @IDRETURN = -1;
             SET @ERRORID = 12;
             SET @ERRORDESCRIPCION = 'La categoría de producto especificada no existe.';
-            RETURN; -- Salir del procedimiento almacenado
+            
         END
+		ELSE
+		BEGIN
+			-- Eliminar la categoría de producto
+			UPDATE TB_CATE_PRODUCTO
+			SET ESTADO = 0
+			WHERE ID_CATE_PRODUCTO = @Id_Categoria_Producto;
 
-        -- Eliminar la categoría de producto
-        UPDATE TB_CATE_PRODUCTO
-        SET ESTADO = 0
-        WHERE ID_CATE_PRODUCTO = @Id_Categoria_Producto;
-
-        SET @IDRETURN = 1; -- Éxito
-
-        COMMIT TRANSACTION 
+			SET @IDRETURN = 1; -- Éxito
+		END
     END TRY
     BEGIN CATCH
         SET @IDRETURN = -1;
         SET @ERRORID = ERROR_NUMBER();
         SET @ERRORDESCRIPCION = ERROR_MESSAGE();
-        ROLLBACK TRANSACTION 
+
+		--Bitacorear error en BD.
+		INSERT INTO TB_ERROR_EN_BASE_DATOS 
+			(
+				NUM_SEVERIVDAD,
+				STORE_PROCEDURE,
+				NUM_ERROR,
+				DSC_DESCRIPCION,
+				NUM_LINEA,
+				FEC_ERROR
+			) 
+			SELECT ERROR_SEVERITY(),
+				   ERROR_PROCEDURE(),
+				   ERROR_NUMBER(),
+				   ERROR_MESSAGE(),
+				   ERROR_LINE(),
+				   GETUTCDATE();
     END CATCH
 END
-GO
