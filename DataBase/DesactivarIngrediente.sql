888720b7a30a25f0e0d7458707eaa63b4efa70d0
JbDolmus <129348901+JbDolmus@users.noreply.github.com>
2024-04-14 12:12:26 -0600

    Update DesactivarIngrediente.sql

--- DataBase/DesactivarIngrediente.sql
+++ DataBase/DesactivarIngrediente.sql
@@ -26,7 +26,6 @@ CREATE PROCEDURE Desactivar_Ingrediente
 AS
 BEGIN
     BEGIN TRY
-        BEGIN TRANSACTION 
 
         -- Verificar si el ingrediente existe
         IF NOT EXISTS (SELECT * FROM TB_INGREDIENTE WHERE ID_INGREDIENTE = @Id_Ingrediente)
@@ -34,24 +33,38 @@ BEGIN
             SET @IDRETURN = -1;
             SET @ERRORID = 12;
             SET @ERRORDESCRIPCION = 'El ingrediente especificado no existe.';
-            RETURN; -- Salir del procedimiento almacenado
         END
+		ELSE
+		BEGIN
+			-- Eliminar el ingrediente
+			UPDATE TB_INGREDIENTE
+			SET ESTADO = 2
+			WHERE ID_INGREDIENTE = @Id_Ingrediente;
 
-        -- Eliminar el ingrediente
-        UPDATE TB_INGREDIENTE
-        SET ESTADO = 2
-        WHERE ID_INGREDIENTE = @Id_Ingrediente;
+			SET @IDRETURN = 1; -- Ã‰xito
 
-        SET @IDRETURN = 1; -- Éxito
-
-        COMMIT TRANSACTION 
+        END
     END TRY
     BEGIN CATCH
         SET @IDRETURN = -1;
         SET @ERRORID = ERROR_NUMBER();
         SET @ERRORDESCRIPCION = ERROR_MESSAGE();
-        ROLLBACK TRANSACTION 
-        -- Aquí puedes manejar el error como prefieras, por ejemplo, lanzar una excepción o registrar el error en una tabla de registro de errores.
+        --Bitacorear error en BD.
+		INSERT INTO TB_ERROR_EN_BASE_DATOS 
+			(
+				NUM_SEVERIVDAD,
+				STORE_PROCEDURE,
+				NUM_ERROR,
+				DSC_DESCRIPCION,
+				NUM_LINEA,
+				FEC_ERROR
+			) 
+			SELECT ERROR_SEVERITY(),
+				   ERROR_PROCEDURE(),
+				   ERROR_NUMBER(),
+				   ERROR_MESSAGE(),
+				   ERROR_LINE(),
+				   GETUTCDATE(); 
     END CATCH
 END
 GO
