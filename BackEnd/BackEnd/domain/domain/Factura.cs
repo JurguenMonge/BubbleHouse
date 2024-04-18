0ffdf6e53428e24f97bfb2e072ee6dcda9de5098
Fernanda35Gonzalez <127688826+Fernanda35Gonzalez@users.noreply.github.com>
2024-04-17 19:44:43 -0600

    clases

---
+++ BackEnd/BackEnd/domain/domain/Factura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.domain
+{
+    public class Factura
+    {
+         public int idFactura {  get; set; }
+         public int idProducto { get; set; }
+         public int idSesion {  get; set; }
+         public float numSubTotal { get; set; }
+         public float numDescuento { get; set; }
+         public float numTotal {  get; set; }
+         public DateTime fecha {  get; set; }
+         public bool estado {  get; set; }
+
+    }
+}
