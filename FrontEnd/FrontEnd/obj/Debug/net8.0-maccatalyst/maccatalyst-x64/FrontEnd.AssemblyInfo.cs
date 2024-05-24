f0da8cc468e57a70e8271f80a5c6466f367e01db
Victor120120 <103450911+Victor120120@users.noreply.github.com>
2024-05-23 20:14:34 -0600

    si

--- FrontEnd/FrontEnd/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/FrontEnd.AssemblyInfo.cs
+++ FrontEnd/FrontEnd/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/FrontEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e8d49b070cd6737aaeea7ccb37e313018369056a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b8a518ce15bf70647d4bb7c31c163232bc8829c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
