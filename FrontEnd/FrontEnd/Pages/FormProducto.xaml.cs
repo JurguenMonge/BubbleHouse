30d06cc566d692f7f889f39b24cab3d929c971a1
Jurguen Monge <82221622+JurguenMonge@users.noreply.github.com>
2024-05-30 19:55:45 -0600

    avance producto

--- FrontEnd/FrontEnd/Pages/FormProducto.xaml.cs
+++ FrontEnd/FrontEnd/Pages/FormProducto.xaml.cs
@@ -2,6 +2,10 @@ namespace FrontEnd.Pages;
 
 public partial class FormProducto : ContentPage
 {
+    public int cateIngredienteId;
+    private bool isPickerOpen = false;
+    string path = "C:\\Users\\Jurguen Monge\\Documents\\GitHub\\BubbleHouse\\FrontEnd\\FrontEnd\\Resources\\Images";
+    public string selectedImagePath;
     public FormProducto()
     {
         InitializeComponent();
@@ -22,8 +26,75 @@ public partial class FormProducto : ContentPage
 
     }
 
-    private void btnEliminar_Clicked(object sender, EventArgs e)
+    private async void btnEliminar_Clicked(object sender, EventArgs e)
+    {
+        bool answer = await DisplayAlert("Confirmación", "¿Estás seguro de eliminar este Producto?", "Aceptar", "Cancelar");
+        if (answer)
+        {
+            //IngredienteController controller = new IngredienteController();
+            try
+            {
+                //ResIngrediente res = new ResIngrediente();
+                //res = await controller.EliminarIngrediente(int.Parse(txtId.Text));
+                //if (res.Resultado)
+                //{
+                //    await DisplayAlert("Eliminación Exitosa", "Ingrediente eliminado con éxito", "Aceptar");
+                //    await Navigation.PopAsync();
+                //}
+                //else
+                //{
+                //    await DisplayAlert("Error en eliminación", "Sucedió un error al eliminar: " + res.ListaDeErrores.First(), "Aceptar");
+                //}
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error interno", "Por favor, reinstale la aplicación", "Aceptar");
+            }
+        }
+    }
+
+    private async void btnSeleccionar_Clicked(object sender, EventArgs e)
     {
 
+        try
+        {
+            var result = await FilePicker.PickAsync(new PickOptions
+            {
+                FileTypes = FilePickerFileType.Images,
+                PickerTitle = "Seleccione una imagen"
+            });
+
+            if (result != null)
+            {
+
+                var stream = await result.OpenReadAsync();
+
+
+                var fileName = Path.GetFileName(result.FullPath);
+
+
+                var imagePath = Path.Combine(path, fileName);
+
+
+                Directory.CreateDirectory(Path.GetDirectoryName(imagePath));
+
+
+                using (var fileStream = new FileStream(imagePath, FileMode.Create, FileAccess.Write))
+                {
+                    await stream.CopyToAsync(fileStream);
+                }
+
+
+                selectedImage.Source = ImageSource.FromStream(() => new FileStream(imagePath, FileMode.Open, FileAccess.Read));
+
+
+                selectedImagePath = fileName;
+
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", $"Ocurrió un error: {ex.Message}", "OK");
+        }
     }
 }
