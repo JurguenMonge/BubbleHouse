596f7cc4ce2e1b786da0d4d204b1a068921790cc
Jurguen Monge <82221622+JurguenMonge@users.noreply.github.com>
2024-06-03 15:21:37 -0600

    commit

--- FrontEnd/FrontEnd/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/FrontEnd.AssemblyInfo.cs
+++ FrontEnd/FrontEnd/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/FrontEnd.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+70c2dd8deddb1d6b5fc537f0d02f2ec8e8381570")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e570288ef79da02d78567e919679103d967493f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
